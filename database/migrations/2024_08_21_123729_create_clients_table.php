6a278cecf4e52794b1b7e70e765269e493f8e0f5
mr-sabya <sabya.citik@gmail.com>
2025-01-20 16:22:19 +0600

    update migrations

--- database/migrations/2024_08_21_123729_create_clients_table.php
+++ database/migrations/2024_08_21_123729_create_clients_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('image');
+            $table->string('phone');
+            $table->string('email')->unique();
+            $table->string('address');
             $table->timestamps();
         });
     }
